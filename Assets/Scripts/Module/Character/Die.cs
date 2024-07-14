2558fa53e78533ea2835545f8bdf92fea06f1791
NiCeXD007 <1642377583@qq.com>
2024-07-14 18:58:51 +0800

    Moving

--- Assets/Scripts/Module/Character/Die.cs
+++ Assets/Scripts/Module/Character/Die.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Die : MonoBehaviour
 {
@@ -11,16 +12,36 @@ public class Die : MonoBehaviour
     public void Kill()
     {
         animator.enabled = false;
+        enemyMovement.enabled = false;
+        navMeshAgent.enabled = false;
+        
         RigidbodySwitch(false);
     }
 
     public bool KillFlag = false;
     private static List<Rigidbody> rigidbodies;
     private static Animator animator;
+    private static NavMeshAgent navMeshAgent;
+    private static EnemyMovement enemyMovement;
     private void OnEnable()
     {
         rigidbodies = new List<Rigidbody>(this.GetComponentsInChildren<Rigidbody>());
         animator = this.GetComponent<Animator>();
+        enemyMovement = this.GetComponent<EnemyMovement>();
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float speed = navMeshAgent.speed;
+        if (speed > 0.1f)
+        {
+            animator.SetInteger("State", 1);
+        }
+        else
+        {
+            animator.SetInteger("State", 0);
+        }
     }
 
     private static void RigidbodySwitch(bool state)
